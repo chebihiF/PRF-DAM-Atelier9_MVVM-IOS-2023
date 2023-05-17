33deb3a5ee7e9b22a81aeef9b9b9d934774ed5ba
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-17 10:21:31 +0100

    Initial Commit

---
+++ Atelier9_MVVM/Atelier9_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atelier9_MVVMApp.swift
+//  Atelier9_MVVM
+//
+//  Created by CHEBIHI FAYCAL on 17/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Atelier9_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atelier9_MVVM/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Atelier9_MVVM
+//
+//  Created by CHEBIHI FAYCAL on 17/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
